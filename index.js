5b4810a9f1c5009b473f9624e17e309e2a964fc9
anupray95 <rayanup95@gmail.com>
2022-09-03 22:18:42 +0530

    github to aws

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/first", (req, res) => res.send("first"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
